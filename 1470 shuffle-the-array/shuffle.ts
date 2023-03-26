2aed6c72a8291c521ef9990c22e313dc200d44e5
Kunanon Srisuntiroj <son9912@gmail.com>
2023-03-26 15:34:42 +0700

    Completed 1470. Shuffle the Array

--- 1470 shuffle-the-array/shuffle.ts
+++ 1470 shuffle-the-array/shuffle.ts
@@ -1,1 +1,8 @@
-function shuffle(nums: number[], n: number): number[] {}
+function shuffle(nums: number[], n: number): number[] {
+    var resultArray = [];
+    for (var i = 0; i < n; i++) {
+        resultArray.push(nums[i]);
+        resultArray.push(nums[i + n]);
+    }
+    return resultArray;
+}
